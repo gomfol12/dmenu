27bd8e45287f814f036e6651f6079622ba5a8a77
gomfol12 <marek@am-hohenstein.de>
2021-02-09 18:58:38 +0100

    changed linehight, font size, fixed a warning

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Iosevka Medium Extended:size=10"
+	"Iosevka Medium Extended:pixelsize=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *symbol_1 = "<-";
@@ -21,7 +21,7 @@ static const char *colors[SchemeLast][2] = {
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 32;
+static unsigned int lineheight = 20;
 static unsigned int min_lineheight = 8;
 
 /*
